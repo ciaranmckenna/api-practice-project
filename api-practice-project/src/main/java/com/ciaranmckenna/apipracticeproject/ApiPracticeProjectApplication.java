8adf506413e7fb54915ff269b6d5b35b5ec50c81
Ciaran McKenna <hello@ciaranmckenna.com>
2022-03-28 14:00:08 +0100

    Uploading maven project structure

---
+++ api-practice-project/src/main/java/com/ciaranmckenna/apipracticeproject/ApiPracticeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ciaranmckenna.apipracticeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPracticeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPracticeProjectApplication.class, args);
+	}
+
+}
---
+++ api-practice-project/src/test/java/com/ciaranmckenna/apipracticeproject/ApiPracticeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciaranmckenna.apipracticeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPracticeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
