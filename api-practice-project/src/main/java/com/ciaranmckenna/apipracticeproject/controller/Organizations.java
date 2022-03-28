a2be0b55261a74d2d0ce5393cd64d77351abdc55
Ciaran McKenna <hello@ciaranmckenna.com>
2022-03-28 14:41:51 +0100

    Organizations end point reached and tested

---
+++ api-practice-project/src/main/java/com/ciaranmckenna/apipracticeproject/controller/Organizations.java
@@ -0,0 +1,11 @@
+package com.ciaranmckenna.apipracticeproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Organizations {
+
+    @GetMapping("/")
+    public String organizations(){ return "organizations";}
+}
