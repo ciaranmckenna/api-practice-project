6c697b22655b3e1ca586bac8cfa711e28f53f788
Ciaran McKenna <hello@ciaranmckenna.com>
2022-03-31 18:56:25 +0100

    Resolved page display bug

--- api-practice-project/src/main/java/com/ciaranmckenna/apipracticeproject/controller/OrganizationController.java
+++ api-practice-project/src/main/java/com/ciaranmckenna/apipracticeproject/controller/OrganizationController.java
@@ -1,11 +1,11 @@
 package com.ciaranmckenna.apipracticeproject.controller;
 
 import com.ciaranmckenna.apipracticeproject.repository.OrganizationRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class OrganizationController {
 
     private final OrganizationRepository organizationRepository;
@@ -16,13 +16,6 @@ public class OrganizationController {
 
     @RequestMapping("/organizations")
     public String getOrganizations(Model model){
-
-        model.addAttribute("organizations", organizationRepository.findAll());
-        return "organizations/list";
-    }
-
-    @RequestMapping("/organizations")
-    public String getOrganizationsById(Model model){
 
         model.addAttribute("organizations", organizationRepository.findAll());
         return "organizations/list";
